24340a47e35cd4e1a19da9e8a920b4945df460ee
flekay <flekay@hotmail.de>
2024-09-09 17:51:53 +0200

    Improved `navi-to-pos.py` path generation from 51.0140s to 9.4829s.

--- movement/navi-to-pos.py
+++ movement/navi-to-pos.py
@@ -1,33 +1,31 @@
-def get_shortest_path(start, end, size, hws):
-	path = []
-
-	dx = (end[0] - start[0] + hws) % size - hws
-	dy = (end[1] - start[1] + hws) % size - hws
-
-	for i in range(dx):
-		path.append(East)
-	for i in range(-dx):
-		path.append(West)
-	for i in range(dy):
-		path.append(North)
-	for i in range(-dy):
-		path.append(South)
-
-	return path
-
-def generate_path_map(size=get_world_size()):
-	hws = size // 2
-	possible_tuples = []
-	for i in range(size):
-		for j in range(size):
-			possible_tuples.append((i, j))
-	# Generate a nested dictionary structure for efficient path lookup
+def generate_path_map(ws=get_world_size()):
+	hws = ws // 2
+	ranges = range(ws)
+	x_paths = {}
+	y_paths = {}
 	map_paths = {}
-	for start in possible_tuples:
-		map_paths[start] = {}
-		for end in possible_tuples:
-			if start == end:
-				map_paths[start][end] = []
-			else:
-				map_paths[start][end] = get_shortest_path(start, end, size, hws)
+
+	for i in ranges:
+		x_paths[i] = {}
+		y_paths[i] = {}
+		for j in ranges:
+			x_paths[i][j] = []
+			y_paths[i][j] = []
+			map_paths[(i, j)] = {}
+			delta = (j - i + hws) % ws - hws
+			for k in range(delta):
+				x_paths[i][j].append(East)
+				y_paths[i][j].append(North)
+			for k in range(-delta):
+				x_paths[i][j].append(West)
+				y_paths[i][j].append(South)
+
+	for start_pos in map_paths:
+		sx, sy = start_pos
+		x_path_start = x_paths[sx]
+		y_path_start = y_paths[sy]
+
+		for end_pos in map_paths:
+			ex, ey = end_pos
+			map_paths[start_pos][end_pos] = x_path_start[ex] + y_path_start[ey]
 	return map_paths
