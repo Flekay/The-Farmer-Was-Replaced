f90444df008735f2998aba0d3a2335048d70f1a5
flekay <flekay@hotmail.de>
2024-10-04 14:11:25 +0200

    added nearest-neighbor.py and distance.py used in power-path.py as general functions

---
+++ general/pathing/nearest-neighbor.py
@@ -0,0 +1,16 @@
+def nearest_neighbor(start_pos, points):
+    path = [start_pos]
+
+    while points:
+        nearest = None
+        nearest_dist = 999
+        for point in points:
+            dist = distance(path[-1], point)
+            if dist < nearest_dist:
+                nearest = point
+                nearest_dist = dist
+
+        path.append(nearest)
+        points.remove(nearest)
+
+    return path
