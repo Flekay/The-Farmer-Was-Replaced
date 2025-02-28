f83348ef2995717c515f9e122d534d8585636f74
flekay <flekay@hotmail.de>
2025-02-28 17:47:24 +0100

    improved popcarrot.py and added strip.py for polyculture carrot

--- polyculture-carrot/README.md
+++ polyculture-carrot/README.md
@@ -4,9 +4,16 @@ This is a collection of scripts that are used to farm carrots using polyculture.
 ## functions
 
 ### popcarrot.py
-I dont know why this only produces so few items, it should be able to produce 316k items/min.
+This script is the same as `pophay.py` but for carrots.
+
+### strip.py
+This plants and harvests a single strip of carrots over and over again.
 
 ## Benchmarks
-| file          | items/min |
-| ------------- | --------- |
-| popcarrot.py  | 147k      |
+benchmarks with "-" have a less than 100% success rate and are therefore not able to compete in leaderboard runs.
+
+### Benchmark Results
+| file         | items/min | leaderboard time |
+| -----------  | --------- | ---------------- |
+| popcarrot.py |   158 000 |         1:13.897 |
+| strip.py     |    70 700 |         1:34.172 |
--- polyculture-carrot/popcarrot.py
+++ polyculture-carrot/popcarrot.py
@@ -1,76 +1,44 @@
-MOVES = generate_moves()
-MOVES_ONE_MIN = generate_moves(2500)
-PUMPKIN = Entities.Pumpkin
-CARROT = Entities.Carrot
-FERTILIZER = Items.Fertilizer
-WATER = Items.Water
-BUSH = Entities.Bush
-BUSH_LOCATIONS = [(1,1),(3,6),(6,3),(9,6),(6,9)]
+clear()
+from map_pos import MOVES
+companions = {}
 
-def poly_carrots():
-	clear()
-	companions = {(1,1):BUSH, (3,6):BUSH, (6,3):BUSH, (9,6):BUSH, (6,9):BUSH}
-	def preplant():
-		for dir in MOVES:
-			till()
-			if (get_pos_x(), get_pos_y()) in BUSH_LOCATIONS:
-				plant(BUSH)
-			else:
-				plant(CARROT)
-				companion, pos = get_companion()
-				while not pos in BUSH_LOCATIONS:
-					harvest()
-					plant(CARROT)
-					companion, pos = get_companion()
-			use_item(WATER)
-			use_item(WATER)
-			use_item(WATER)
-			use_item(WATER)
-			move(dir)
+for dir, coords in MOVES:
+	till()
+	if coords in companions:
+		plant(companions.pop(coords))
+		move(dir)
+	else:
+		plant(Entities.Carrot)
+		companion, pos = get_companion()
+		while pos in companions and companion != companions[pos]:
+			harvest()
+			plant(Entities.Carrot)
+			companion, pos = get_companion()
+		companions[pos] = companion
+		move(dir)
 
-	def tillall():
-		for dir in MOVES:
-			till()
-			move(dir)
-	def prewater():
-		for dir in MOVES:
-			use_item(WATER)
-			use_item(WATER)
-			move(dir)
-
-	preplant()
-	# tillall()
-	# prewater()
-	prewater()
-
-	# start = get_time()
-	for dir in MOVES_ONE_MIN:
-		coords = (get_pos_x(), get_pos_y())
+while num_items(Items.Carrot) < 100000:
+	for dir, coords in MOVES:
 		if coords in companions:
-			if get_entity_type() == companions[coords]:
+			comp = companions.pop(coords)
+			if get_entity_type() == comp:
 				move(dir)
 			else:
 				harvest()
-				plant(companions.pop(coords))
+				plant(comp)
 				move(dir)
 		else:
-
+			if get_entity_type() == Entities.Carrot:
+				while not can_harvest():
+					if get_water() < 0.5:
+						use_item(Items.Water)
+					use_item(Items.Fertilizer)
 			harvest()
-			plant(CARROT)
+			plant(Entities.Carrot)
 			companion, pos = get_companion()
-			# while pos in companions:
-			# 	harvest()
-			# 	plant(CARROT)
-			# 	companion, pos = get_companion()
-
+			while pos in companions and companion != companions[pos]:
+				harvest()
+				plant(Entities.Carrot)
+				companion, pos = get_companion()
 			companions[pos] = companion
 			move(dir)
-	# quick_print("Time taken: " + str(get_time() - start))
-
-	for dir in MOVES:
-		if get_entity_type() == CARROT:
-			harvest()
-		move(dir)
-
-while True:
-	poly_carrots()
